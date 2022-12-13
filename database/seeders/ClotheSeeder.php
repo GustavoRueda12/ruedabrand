499ec7886e6cba887ed0a6e30c531928b0b2d9c8
Gustavo <93703992+GustavoRueda12@users.noreply.github.com>
2022-12-13 23:49:06 +0100

    Update ClotheSeeder.php

--- database/seeders/ClotheSeeder.php
+++ database/seeders/ClotheSeeder.php
@@ -25,10 +25,26 @@ class ClotheSeeder extends Seeder
 
         Clothe::create([
             'id'=> '2',
+            'collection_id'=> '1',
+            'clothe_name'=> 'Calcetines WORLD CHAMPION',
+            'price'=>'7.99',
+            'description'=>'Calentitos, como el campeón dejó a sus rivales.',
+        ]);
+
+        Clothe::create([
+            'id'=> '3',
             'collection_id'=> '2',
             'clothe_name'=> 'Camiseta KTM AJO',
             'price'=>'14.99',
-            'description'=>'Cómodo, suave, sencillo y práctico. No incluimos la bandolera. El toque cani ya es cosa tuya, no nuestra.',
+            'description'=>'Camiseta de la nueva temporada.',
+        ]);
+
+        Clothe::create([
+            'id'=> '4',
+            'collection_id'=> '2',
+            'clothe_name'=> 'Sudadera KTM AJO',
+            'price'=>'19.99',
+            'description'=>'Sudadera con gorro y bolsillo de canguro que me trajo mi amigo de australia.',
         ]);
     }
 }
